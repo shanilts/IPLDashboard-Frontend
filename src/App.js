6c9f563695e0d84e166adc5af09c8c21aa9f4658
shanil <shanilts123@gmail.com>
2022-08-14 19:23:13 +0530

    added March Page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { TeamPage } from './pages/TeamPage';
 import { BrowserRouter as RootRouter,Routes, Route } from "react-router-dom";
+import { MatchPage } from './pages/MatchPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <RootRouter>
         <Routes>
           <Route path="/teams/:teamName" element={<TeamPage />} />
+          <Route path="/teams/:teamName/matches/:year" element={<MatchPage />} />
         </Routes>
       </RootRouter>
 
